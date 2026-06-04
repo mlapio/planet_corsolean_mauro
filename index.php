35c2923e7375c8d14609b44a8c8a5a4b2a37bb35
Mauro Lapio <m.lapio@planetweb.it>
2026-06-04 17:47:59 +0200

    feat: Aggiunto progetto PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+    function test($t) {
+        return "test!: $t" 
+    }
+
+?>
+
+<html>
+    <head>New project!</head>
+    <body>
+        <?php echo test(prova); ?>
+    </body>
+</html>
